eb0838d71874ac6b15f4d3e5aa268bcbb4be6050
zl90 <101400977+zl90@users.noreply.github.com>
2022-08-02 14:48:36 +1000

    Add boilerplate Timestamp to Comment items

--- src/components/Content/Article/Comments/CommentList/CommentItem/CommentItem.js
+++ src/components/Content/Article/Comments/CommentList/CommentItem/CommentItem.js
@@ -1,11 +1,13 @@
 import Name from "./Name/Name";
 import Content from "./Content/Content";
+import Timestamp from "./Timestamp/Timestamp";
 
 const CommentItem = () => {
   return (
     <div className="article-comment-item-container">
       <Name />
       <Content />
+      <Timestamp />
     </div>
   );
 };
---
+++ src/components/Content/Article/Comments/CommentList/CommentItem/Timestamp/Timestamp.js
@@ -0,0 +1,7 @@
+const Timestamp = () => {
+  return (
+    <div className="article-comment-timestamp-container">Posted 4h ago</div>
+  );
+};
+
+export default Timestamp;
